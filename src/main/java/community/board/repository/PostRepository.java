a5abf1075d7f5a31af6352df420bafcb73a5e3c0
James <74996516+chungjm@users.noreply.github.com>
2022-05-11 02:11:45 +0900

    feat : add security dependency

--- src/main/java/community/board/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package community.board.repository;
-
-import community.board.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
