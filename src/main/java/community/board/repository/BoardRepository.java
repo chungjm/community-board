9e19d31c6d9da7df199bd6351f7c7eae44cf3600
James <74996516+chungjm@users.noreply.github.com>
2022-05-12 20:59:27 +0900

    feat: board dto, repository

---
+++ src/main/java/community/board/repository/BoardRepository.java
@@ -0,0 +1,17 @@
+package community.board.repository;
+
+import community.board.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Modifying
+    @Query("update Board b set b.count = b.count + 1 where b.id = :id")
+    int updateCount(Long id);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+}
