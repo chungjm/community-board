5cc82acbd8d7f96b8c4bb7ec9edfbd6aa42faa27
James <74996516+chungjm@users.noreply.github.com>
2022-05-12 20:59:02 +0900

    feat: 게시판 도메인 및 컨트롤러

--- src/main/java/community/board/controller/BoardController.java
+++ src/main/java/community/board/controller/BoardController.java
@@ -1,15 +1,35 @@
 package community.board.controller;
 
+import community.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
 
-    @GetMapping("/list")
-    public String list() {
-        return "board/list";
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String boardSave() {
+        return "/board/board-save";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        model.addAttribute("board", boardService.detail(id));
+        boardService.updateCount(id);
+        return "/board/board-detail";
+    }
+
+    @GetMapping("/{id}/update")
+    public String update(@PathVariable Long id, Model model) {
+        model.addAttribute("board", boardService.detail(id));
+        return "/board/board-update";
     }
 }
---
+++ src/main/java/community/board/domain/Board.java
@@ -0,0 +1,46 @@
+package community.board.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "board_id")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Column
+    private int count;  //게시글 조회수
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OrderBy("id desc")
+    @JsonIgnoreProperties({"board"})
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)
+    private List<Reply> replyList = new ArrayList<>();
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
