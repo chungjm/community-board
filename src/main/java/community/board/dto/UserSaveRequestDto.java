7d93c498953930428bc0db52f392b438275779d7
James <74996516+chungjm@users.noreply.github.com>
2022-05-12 18:59:11 +0900

    feat: UserSaveRequestDto

---
+++ src/main/java/community/board/dto/UserSaveRequestDto.java
@@ -0,0 +1,33 @@
+package community.board.dto;
+
+import community.board.domain.User;
+import community.board.domain.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class UserSaveRequestDto {
+
+    private String username;
+    private String password;
+    private String email;
+    private String nickname;
+
+    private Role role;
+
+    public User toEntity() {
+        return User.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .nickname(nickname)
+                .role(Role.USER)
+                .build();
+    }
+
+}
